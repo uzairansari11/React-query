db20d6a5f4daad9bc5918bdb81d2262e62af91d6
Uzair <uzair@talkwisely.io>
2024-12-01 20:26:52 +0530

    first commit

--- src/component/post/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./post";
-export * from "./post-card";
-export * from "./post-action-button"
--- src/provider/react-query-provider.jsx
+++
@@ -1,9 +0,0 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
-export const ReactQueryProvider = ({ children }) => {
-	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-	);
-};
